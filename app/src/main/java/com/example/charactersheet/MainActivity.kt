44142c3dcfd3f55d62ce835c7c242d18560b5744
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-03-10 16:32:21 +0200

    artist list

--- app/src/main/java/com/example/charactersheet/MainActivity.kt
+++ app/src/main/java/com/example/charactersheet/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.charactersheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.charactersheet.model.Artist
 import com.example.charactersheet.ui.theme.CharacterSheetTheme
-
+import androidx.compose.foundation.lazy.items
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +47,43 @@ fun CharacterSheetApp() {
 
 }
 
+@Composable
+fun ArtistList(artistList: List<Artist>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(artistList) { artist ->
+            ArtistCard(
+                artist = artist,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun ArtistCard(artist: Artist, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(artist.imageResourceId),
+                contentDescription = stringResource(artist.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(192.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = LocalContext.current.getString(artist.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CharacterSheetTheme {
-       
+
     }
 }
--- app/src/main/java/com/example/charactersheet/data/Datasource.kt
+++ app/src/main/java/com/example/charactersheet/data/Datasource.kt
@@ -1,4 +1,35 @@
 package com.example.charactersheet.data
 
-class Datasource {
+import com.example.charactersheet.R
+import com.example.charactersheet.model.Artist
+import com.example.charactersheet.model.Character
+class Datasource() {
+    fun loadArtists(): List<Artist> {
+        return listOf<Artist>(
+//            Artist(R.string.affirmation1, R.drawable.image1),
+//            Artist(R.string.affirmation2, R.drawable.image2),
+//            Artist(R.string.affirmation3, R.drawable.image3),
+//            Artist(R.string.affirmation4, R.drawable.image4),
+//            Artist(R.string.affirmation5, R.drawable.image5),
+//            Artist(R.string.affirmation6, R.drawable.image6),
+//            Artist(R.string.affirmation7, R.drawable.image7),
+//            Artist(R.string.affirmation8, R.drawable.image8),
+//            Artist(R.string.affirmation9, R.drawable.image9),
+//            Artist(R.string.affirmation10, R.drawable.image10)
+                )
+    }
+    fun loadCharacters(): List<Artist> {
+        return listOf<Artist>(
+//            Artist(R.string.affirmation1, R.drawable.image1),
+//            Artist(R.string.affirmation2, R.drawable.image2),
+//            Artist(R.string.affirmation3, R.drawable.image3),
+//            Artist(R.string.affirmation4, R.drawable.image4),
+//            Artist(R.string.affirmation5, R.drawable.image5),
+//            Artist(R.string.affirmation6, R.drawable.image6),
+//            Artist(R.string.affirmation7, R.drawable.image7),
+//            Artist(R.string.affirmation8, R.drawable.image8),
+//            Artist(R.string.affirmation9, R.drawable.image9),
+//            Artist(R.string.affirmation10, R.drawable.image10)
+    )
+    }
 }
