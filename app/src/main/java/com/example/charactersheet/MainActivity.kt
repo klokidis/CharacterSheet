d5a1fe52d0e3dc67a316dbf7a8086666268213d6
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-03-10 22:42:28 +0200

    create first artist

--- app/src/main/java/com/example/charactersheet/MainActivity.kt
+++ app/src/main/java/com/example/charactersheet/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.charactersheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +28,13 @@ import androidx.compose.ui.unit.dp
 import com.example.charactersheet.model.Artist
 import com.example.charactersheet.ui.theme.CharacterSheetTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.example.charactersheet.data.Datasource
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,37 +54,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CharacterSheetApp() {
-
+    ArtistList(Datasource().loadArtists())
 }
 
 @Composable
 fun ArtistList(artistList: List<Artist>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(artistList) { artist ->
-            ArtistCard(
-                artist = artist,
-                modifier = Modifier.padding(8.dp)
-            )
+    Column(Modifier
+        .padding(top = 5.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = LocalContext.current.getString(R.string.Artists),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        LazyColumn(modifier = modifier) {
+            items(artistList) { artist ->
+                ArtistCard(
+                    artist = artist,
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
 
 @Composable
 fun ArtistCard(artist: Artist, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Column {
+    Card(modifier = modifier,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        )) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
             Image(
                 painter = painterResource(artist.imageResourceId),
                 contentDescription = stringResource(artist.stringResourceId),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(192.dp),
+                    .size(width = 140.dp, height = 130.dp)
+                    .padding(7.dp)
+                    .clip(RoundedCornerShape(percent = 10)),
                 contentScale = ContentScale.Crop
             )
             Text(
                 text = LocalContext.current.getString(artist.stringResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
+                modifier = Modifier.padding(start = 10.dp, end = 16.dp),
+                style = MaterialTheme.typography.titleLarge
             )
         }
     }
@@ -84,6 +110,6 @@ fun ArtistCard(artist: Artist, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CharacterSheetTheme {
-
+        ArtistCard(Artist(R.string.name1,R.drawable.artist1) )
     }
 }
--- app/src/main/java/com/example/charactersheet/data/Datasource.kt
+++ app/src/main/java/com/example/charactersheet/data/Datasource.kt
@@ -6,7 +6,13 @@ import com.example.charactersheet.model.Character
 class Datasource() {
     fun loadArtists(): List<Artist> {
         return listOf<Artist>(
-//            Artist(R.string.affirmation1, R.drawable.image1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
+            Artist(R.string.name1, R.drawable.artist1),
 //            Artist(R.string.affirmation2, R.drawable.image2),
 //            Artist(R.string.affirmation3, R.drawable.image3),
 //            Artist(R.string.affirmation4, R.drawable.image4),
@@ -16,7 +22,7 @@ class Datasource() {
 //            Artist(R.string.affirmation8, R.drawable.image8),
 //            Artist(R.string.affirmation9, R.drawable.image9),
 //            Artist(R.string.affirmation10, R.drawable.image10)
-                )
+        )
     }
     fun loadCharacters(): List<Artist> {
         return listOf<Artist>(
